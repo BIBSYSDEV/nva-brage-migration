e5fe70119f902db98f9ecf9005d7663920864b1d
anetteOlli <anette.olli.siiri@sikt.no>
2022-09-29 13:45:02 +0200

    Implemented

--- src/main/java/no/sikt/nva/Main.java
+++ src/main/java/no/sikt/nva/Main.java
@@ -1,11 +1,14 @@
 package no.sikt.nva;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("PMD.ShortClassName")
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //Scaffolding
+        logger.info("Hello world");
     }
 }
