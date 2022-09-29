0666c1e5cbd2b2cd17f742555049dc5b88162004
anetteOlli <anette.olli.siiri@sikt.no>
2022-09-29 10:22:35 +0200

    dummy tests

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import no.sikt.nva.Main;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void runningMain() {
+        var main = new Main();
+        var arguments = new String[]{};
+        main.main(arguments);
+    }
+}
