e34012c2eb83fd60d33a81176782d1455b5a7bcc
anetteOlli <anette.olli.siiri@sikt.no>
2022-10-03 15:17:14 +0200

    NP-14750-scrape-handler, functionality implemented

--- src/main/java/no/sikt/nva/HandleScraper.java
+++ src/main/java/no/sikt/nva/HandleScraper.java
@@ -8,37 +8,52 @@ import no.sikt.nva.model.DcValue;
 import no.sikt.nva.model.DublinCore;
 import no.sikt.nva.model.Element;
 import no.sikt.nva.model.Qualifier;
+import nva.commons.core.StringUtils;
+import nva.commons.core.ioutils.IoUtils;
 import nva.commons.core.paths.UriWrapper;
 
 public class HandleScraper {
 
-    //"dublin_core.xml" and "handle" both contains the handle.
-    //Check dublin_core.xml first for handle and verify that the handle is in the correct format.
-    // Something fails during handle scraping, retort to using the handle file.
+    public static final String ERROR_MESSAGE_NO_HANDLE_IN_DUBLIN_CORE = "No handle present in dublin_core.xml";
+    public static final String ERROR_MESSAGE_HANDLE_IN_DUBLIN_CORE_IS_MALFORMED = "Handle in dublin_core.xml is "
+                                                                                  + "invalid: %s";
+
+    private static final URI HANDLE_DOMAIN = UriWrapper.fromHost("https://hdl.handle.net").getUri();
 
     public static URI extractHandleFromDublinCore(final DublinCore dublinCore) {
-        var handleDcValue =
-            dublinCore.getDcValues().stream().filter(dcValue -> isIdentifierAndUri(dcValue)).findFirst();
-        var handle = handleDcValue.orElseThrow(() -> new HandleException("No handle"
-                                                                         + " "
-                                                                         + "present"
-                                                                         + " in "
-                                                                         + "Doublin"
-                                                                         + " Core"));
-        var handleString = handle.getValue();
+        var dcValueHandle = extractDvValueContainingHandleFromDublinCore(dublinCore);
+        var handleString = dcValueHandle.getValue();
 
         return verifiedHandleURI(handleString);
     }
 
-    public static URI extractHandleFromHandleFile(Path handlePath) {
-        URI handle = null;
+    public static URI extractHandleFromHandlePath(Path handlePath) {
+        String handleSubPath = IoUtils.stringFromResources(handlePath).trim();
+        var handle = UriWrapper.fromUri(HANDLE_DOMAIN).addChild(handleSubPath).getUri();
+        verifyHandle(handle);
         return handle;
     }
 
+    private static DcValue extractDvValueContainingHandleFromDublinCore(DublinCore dublinCore) {
+        return dublinCore.getDcValues()
+                   .stream()
+                   .filter(HandleScraper::isIdentifierAndUri)
+                   .findFirst()
+                   .orElseThrow(() -> new HandleException(ERROR_MESSAGE_NO_HANDLE_IN_DUBLIN_CORE));
+    }
+
     private static URI verifiedHandleURI(String handleString) {
-        var handleUriWrapper = UriWrapper.fromUri(handleString);
-        assert handleUriWrapper.getParent().isPresent();
-        return handleUriWrapper.getUri();
+        var handle = UriWrapper.fromUri(handleString).getUri();
+        verifyHandle(handle);
+        return handle;
+    }
+
+    private static void verifyHandle(final URI handle) {
+        if (!handle.getAuthority().equals(HANDLE_DOMAIN.getAuthority())
+            || StringUtils.isBlank(handle.getPath())) {
+            throw new HandleException(String.format(ERROR_MESSAGE_HANDLE_IN_DUBLIN_CORE_IS_MALFORMED,
+                                                    handle));
+        }
     }
 
     private static boolean isIdentifierAndUri(DcValue dcValue) {
--- src/test/java/HandleScraperTest.java
+++ src/test/java/HandleScraperTest.java
@@ -1,23 +1,27 @@
+import static no.sikt.nva.HandleScraper.ERROR_MESSAGE_HANDLE_IN_DUBLIN_CORE_IS_MALFORMED;
+import static no.sikt.nva.HandleScraper.ERROR_MESSAGE_NO_HANDLE_IN_DUBLIN_CORE;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.nio.file.Path;
 import java.util.List;
 import no.sikt.nva.HandleScraper;
+import no.sikt.nva.exceptions.HandleException;
 import no.sikt.nva.model.DcValue;
 import no.sikt.nva.model.DublinCore;
 import no.sikt.nva.model.Element;
 import no.sikt.nva.model.Qualifier;
 import nva.commons.core.paths.UriWrapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class HandleScraperTest {
 
-    private final Path dublin_core_xml_with_handle = Path.of("dublin_core.xml");
-    private final Path handle_file = Path.of("handle");
-    private final Path not_a_dublin_core_file = Path.of("handle");
-    private final Path dublin_core_xml_without_handle = Path.of("dublin_core_without_handle.xml");
-    private HandleScraper handleScraper;
+    private final Path handleFile = Path.of("handle");
 
     @Test
     void shouldReturnHandleFromDublinCoreIfDublinCoreHasHandle() {
@@ -28,7 +32,38 @@ public class HandleScraperTest {
         assertThat(actualHandle, is(equalTo(expectedHandle)));
     }
 
-    private DublinCore generateDublinCoreWithhandle(String handleURI){
+    @ParameterizedTest
+    @ValueSource(strings = {"https://example.com/11250/2684299", "https://hdl.handle.net"})
+    void shouldThrowExceptionWhenhandleInDublinCoreIsMalformed(String malformedHandle) {
+        var dublinCoreWithHandle = generateDublinCoreWithhandle(malformedHandle);
+        var exception = assertThrows(HandleException.class,
+                                     () -> HandleScraper.extractHandleFromDublinCore(dublinCoreWithHandle));
+        assertThat(exception.getMessage(),
+                   containsString(String.format(ERROR_MESSAGE_HANDLE_IN_DUBLIN_CORE_IS_MALFORMED, malformedHandle)));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDublinCoreIsMissingHandle() {
+        var dublinCoreWithouthHandle = generateDublinCoreWithoutHandle();
+        var exception = assertThrows(HandleException.class,
+                                     () -> HandleScraper.extractHandleFromDublinCore(dublinCoreWithouthHandle));
+        assertThat(exception.getMessage(),
+                   containsString(ERROR_MESSAGE_NO_HANDLE_IN_DUBLIN_CORE));
+    }
+
+    @Test
+    void shouldBeAbleToExtractHandleFromHandleFile() {
+        var expectedHandle = UriWrapper.fromUri("https://hdl.handle.net/11250/2684299").getUri();
+        var actualHandle = HandleScraper.extractHandleFromHandlePath(handleFile);
+        assertThat(actualHandle, is(equalTo(expectedHandle)));
+    }
+
+    private DublinCore generateDublinCoreWithoutHandle() {
+        var dcValue = new DcValue(Element.CONTRIBUTOR, Qualifier.AUTHOR, randomString());
+        return new DublinCore(List.of(dcValue));
+    }
+
+    private DublinCore generateDublinCoreWithhandle(String handleURI) {
         var dcValue = new DcValue(Element.IDENTIFIER, Qualifier.URI, handleURI);
         return new DublinCore(List.of(dcValue));
     }
