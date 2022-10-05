b8e6bf6e0bf9307ea0a902442170adc534932be3
anetteOlli <anette.olli.siiri@sikt.no>
2022-10-05 13:57:19 +0200

    NP-14750-scrape-handler, fixed bug in BrageProcessorFactory

--- src/main/java/no/sikt/nva/BrageProcessorFactory.java
+++ src/main/java/no/sikt/nva/BrageProcessorFactory.java
@@ -4,8 +4,7 @@ import nva.commons.core.StringUtils;
 
 public final class BrageProcessorFactory {
 
-    private static final String FILE_TYPE_EXTENSION_DELIMITER = ".";
-    private static final int FILENAME_PART = 0;
+    public static final String ZIP_EXTENSION = ".zip";
     private static final String INVALID_ZIPFILE_NAME_EXCEPTION_MESSAGE = "invalid zipfile name";
 
     private BrageProcessorFactory() {
@@ -13,7 +12,7 @@ public final class BrageProcessorFactory {
     }
 
     public static BrageProcessor createBrageProcessor(String zipfile, String customerId) {
-        var destinationDirectory = zipfile.split(FILE_TYPE_EXTENSION_DELIMITER)[FILENAME_PART];
+        var destinationDirectory = zipfile.replace(ZIP_EXTENSION, StringUtils.EMPTY_STRING);
         if (StringUtils.isEmpty(destinationDirectory)) {
             throw new RuntimeException(INVALID_ZIPFILE_NAME_EXCEPTION_MESSAGE);
         }
