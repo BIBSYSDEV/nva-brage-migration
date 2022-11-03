aff922999e1d4342d481940ddc951695ce01d7d4
Kir Truhacev <kir.truhacev@sikt.no>
2022-11-03 15:18:44 +0100

    validate-journalArticle-in-kanalregister, code review

--- src/main/java/no/sikt/nva/channelregister/ChannelRegister.java
+++ src/main/java/no/sikt/nva/channelregister/ChannelRegister.java
@@ -11,7 +11,6 @@ import no.sikt.nva.model.BrageLocation;
 import no.sikt.nva.model.dublincore.DublinCore;
 import no.sikt.nva.scrapers.DublinCoreScraper;
 import nva.commons.core.SingletonCollector;
-import nva.commons.core.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +40,7 @@ public final class ChannelRegister {
     }
 
     public String lookUpInJournalByIssn(String issn) {
-        return StringUtils.isEmpty(issn) ? channelRegisterJournals.stream()
+        return isNotNullOrEmpty(issn) ? channelRegisterJournals.stream()
                                             .filter(item -> item.hasIssn(issn))
                                             .map(ChannelRegisterJournal::getIdentifier)
                                             .collect(SingletonCollector.collectOrElse(null)) : null;
