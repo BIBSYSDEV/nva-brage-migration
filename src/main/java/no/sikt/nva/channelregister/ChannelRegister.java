f71ba4ec889f376471250bc6315f6515e76d1691
Kir Truhacev <kir.truhacev@sikt.no>
2022-11-04 13:14:31 +0100

    validate-publisher-kanalregister, implemented

--- src/main/java/no/sikt/nva/channelregister/ChannelRegister.java
+++ src/main/java/no/sikt/nva/channelregister/ChannelRegister.java
@@ -9,6 +9,7 @@ import java.util.List;
 import no.sikt.nva.BrageProcessor;
 import no.sikt.nva.model.BrageLocation;
 import no.sikt.nva.model.dublincore.DublinCore;
+import no.sikt.nva.model.record.Record;
 import no.sikt.nva.scrapers.DublinCoreScraper;
 import nva.commons.core.SingletonCollector;
 import org.slf4j.Logger;
@@ -18,13 +19,17 @@ public final class ChannelRegister {
 
     public static final String KANALREGISTER_READING_ERROR_MESSAGE = "Fatal error, could not read kanalregister";
     private static final String JOURNAL_PATH = "journals.csv";
+    private static final String PUBLISHERS_PATH = "publishers.csv";
     private static final char SEPARATOR = ';';
     private static final Logger logger = LoggerFactory.getLogger(BrageProcessor.class);
+    public static final String NOT_FOUND_IN_CHANNEL_REGISTER = "NOT_FOUND_IN_CHANNEL_REGISTER";
     /*volatile*/ private static ChannelRegister register;
     private final List<ChannelRegisterJournal> channelRegisterJournals;
+    private final List<ChannelRegisterPublisher> channelRegisterPublishers;
 
     private ChannelRegister() {
         this.channelRegisterJournals = getJournalsFromCsv();
+        this.channelRegisterPublishers = getPublishersFromCsv();
     }
 
     public static ChannelRegister getRegister() {
@@ -37,6 +42,48 @@ public final class ChannelRegister {
             }
         }
         return register;
+    }
+
+    public static List<ChannelRegisterPublisher> getPublishersFromCsv() {
+        try (var inputStream = Thread.currentThread().getContextClassLoader()
+                                   .getResourceAsStream(PUBLISHERS_PATH);
+            var bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            var microJournal = new CsvToBeanBuilder<ChannelRegisterPublisher>(bufferedReader)
+                                   .withSeparator(SEPARATOR)
+                                   .withType(ChannelRegisterPublisher.class)
+                                   .build();
+            return microJournal.parse();
+        } catch (IOException e) {
+            logger.error(KANALREGISTER_READING_ERROR_MESSAGE);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String lookUpInChannelRegister(Record record) {
+        var publisher = record.getPublication().getPublisher();
+        var issn = record.getPublication().getIssn();
+        if (isNotNullOrEmpty(publisher) && isNotNullOrEmpty(issn)) {
+            var identifierFromJournal = lookUpInJournalByIssn(issn);
+            if (isNotNullOrEmpty(identifierFromJournal)) {
+                return identifierFromJournal;
+            }
+        }
+        var isbn = record.getPublication().getIsbn();
+        if (isNotNullOrEmpty(publisher) && isNotNullOrEmpty(isbn)) {
+            var identifierFromPublisher = lookUpInPublisherByIsbn(isbn);
+            if (isNotNullOrEmpty(identifierFromPublisher)) {
+                return identifierFromPublisher;
+            }
+        }
+        logger.warn(NOT_FOUND_IN_CHANNEL_REGISTER);
+        return null;
+    }
+
+    public String lookUpInPublisherByIsbn(String isbn) {
+        return isNotNullOrEmpty(isbn) ? channelRegisterPublishers.stream()
+                                            .filter(item -> item.hasIsbn(isbn))
+                                            .map(ChannelRegisterPublisher::getIdentifier)
+                                            .collect(SingletonCollector.collectOrElse(null)) : null;
     }
 
     public String lookUpInJournalByIssn(String issn) {
@@ -57,12 +104,11 @@ public final class ChannelRegister {
         var issn = DublinCoreScraper.extractIssn(dublinCore, brageLocation);
         var title = DublinCoreScraper.extractJournal(dublinCore);
 
-        return isNotNullOrEmpty(issn) || isNotNullOrEmpty(title) ? channelRegisterJournals.stream()
-                                                                       .filter(
-                                                                           item -> item.hasIssn(issn) || item.hasTitle(
-                                                                               title))
-                                                                       .map(ChannelRegisterJournal::getIdentifier)
-                                                                       .collect(SingletonCollector.collectOrElse(null))
+        return isNotNullOrEmpty(issn) || isNotNullOrEmpty(title)
+                   ? channelRegisterJournals.stream()
+                         .filter(item -> item.hasIssn(issn) || item.hasTitle(title))
+                         .map(ChannelRegisterJournal::getIdentifier)
+                         .collect(SingletonCollector.collectOrElse(null))
                    : null;
     }
 
---
+++ src/main/java/no/sikt/nva/channelregister/ChannelRegisterPublisher.java
@@ -0,0 +1,57 @@
+package no.sikt.nva.channelregister;
+
+import com.opencsv.bean.CsvBindByName;
+import java.util.Optional;
+import nva.commons.core.StringUtils;
+
+public class ChannelRegisterPublisher {
+
+    @CsvBindByName(column = "Forlag id")
+    private String identifier;
+    @CsvBindByName(column = "Original tittel")
+    private String originalTitle;
+    @CsvBindByName(column = "Internasjonal tittel")
+    private String internationalTitle;
+    @CsvBindByName(column = "ISBN-prefix")
+    private String isbnPrefix;
+
+    public ChannelRegisterPublisher() {
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getInternationalTitle() {
+        return internationalTitle;
+    }
+
+    public void setInternationalTitle(String internationalTitle) {
+        this.internationalTitle = internationalTitle;
+    }
+
+    public String getIsbnPrefix() {
+        return isbnPrefix;
+    }
+
+    public void setIsbnPrefix(String isbnPrefix) {
+        this.isbnPrefix = isbnPrefix;
+    }
+
+    public boolean hasIsbn(String isbn) {
+        var isbnPrefix = Optional.ofNullable(this.getIsbnPrefix()).orElse(StringUtils.EMPTY_STRING);
+        return isbnPrefix.equals(isbn);
+    }
+}
