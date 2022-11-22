2ca0da5b30ffb4af93f360fcd13fb05c045f43f0
Kir Truhacev <kir.truhacev@sikt.no>
2022-11-22 14:21:28 +0100

    NP-17553-s3-storage, implemented

--- post-to-aws/src/main/java/no/sikt/nva/brage/migration/aws/S3RecordStorage.java
+++ post-to-aws/src/main/java/no/sikt/nva/brage/migration/aws/S3RecordStorage.java
@@ -12,16 +12,14 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 import no.sikt.nva.brage.migration.common.model.record.Record;
 import no.sikt.nva.brage.migration.common.model.record.content.ContentFile;
-import no.unit.nva.commons.json.JsonUtils;
-import nva.commons.core.JacocoGenerated;
 import nva.commons.core.SingletonCollector;
+import nva.commons.core.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-@JacocoGenerated
 public class S3RecordStorage implements StoreRecord {
 
     public static final String COULD_NOT_WRITE_MESSAGE = "Could not write files to s3 for: ";
@@ -30,6 +28,12 @@ public class S3RecordStorage implements StoreRecord {
     private static final String bucketName = "anette-kir-brage-migration-experiment";
     private static final Logger logger = LoggerFactory.getLogger(S3RecordStorage.class);
     private final S3Client s3Client;
+    private String pathPrefixString = StringUtils.EMPTY_STRING;
+
+    public S3RecordStorage(S3Client s3Client, String pathPrefixString) {
+        this.s3Client = s3Client;
+        this.pathPrefixString = pathPrefixString;
+    }
 
     public S3RecordStorage(S3Client s3Client) {
         this.s3Client = s3Client;
@@ -37,33 +41,16 @@ public class S3RecordStorage implements StoreRecord {
 
     @Override
     public void storeRecord(Record record) {
-        var filesToStore = getFilesToStore(record);
-        var collection = getCollectionDirectory(record.getBrageLocation()).getName();
-        var bundle = getResourceDirectoryName(record.getBrageLocation());
-        var hardCodedCustomer = "nve";
         try {
-            var recordToStore = convertSingleRecordToJsonString(record);
-            for (UUID fileId : filesToStore.keySet()) {
-                var fileKey = Path.of(hardCodedCustomer, collection, bundle, fileId.toString()).toString();
-                var fileName = filesToStore.get(fileId).getName();
-                s3Client.putObject(PutObjectRequest
-                                       .builder()
-                                       .bucket(bucketName)
-                                       .contentDisposition(fileName)
-                                       .key(fileKey).build(),
-                                   RequestBody.fromFile(filesToStore.get(fileId)));
-            }
-            var recordKey = Path.of(hardCodedCustomer, collection, bundle, extractHandleForFilename(record)).toString();
-            s3Client.putObject(PutObjectRequest
-                                   .builder()
-                                   .bucket(bucketName)
-                                   .contentType(APPLICATION_JSON)
-                                   .key(recordKey)
-                                   .build(),
-                               RequestBody.fromString(recordToStore));
+            writeRecordFilesToS3(record);
+            writeRecordToS3(record);
         } catch (Exception e) {
             logger.info(COULD_NOT_WRITE_MESSAGE + record.getBrageLocation() + " " + e.getMessage());
         }
+    }
+
+    public String getPathPrefixString() {
+        return pathPrefixString;
     }
 
     private static String extractHandleForFilename(Record record) {
@@ -75,38 +62,75 @@ public class S3RecordStorage implements StoreRecord {
         return brageLocation.split("/")[1];
     }
 
-    private static File getCollectionDirectory(String brageLocation) {
-        return new File(brageLocation.split("/")[0]);
-    }
-
-    private static String convertSingleRecordToJsonString(Record record) throws JsonProcessingException {
-        return JsonUtils.dtoObjectMapper.writeValueAsString(record);
-    }
-
-    private static UUID getUuid(Record record, File file) {
+    private static UUID getUuidToRecordFile(Record record, File file) {
         return record.getContentBundle().getContentFileByFilename(file.getName()).getIdentifier();
     }
 
-    private Map<UUID, File> getFilesToStore(Record record) {
+    private File findFilesToRecord(Record record) {
         var brageLocation = record.getBrageLocation();
         var collectionDirectory = getCollectionDirectory(brageLocation);
         var resourceDirectoryName = brageLocation.split("/")[1];
-        var resourceFiles = Arrays.stream(Objects.requireNonNull(collectionDirectory.listFiles()))
-                                .filter(resourceDir -> resourceDir.getName().equals(resourceDirectoryName))
-                                .collect(SingletonCollector.collect());
+        return Arrays.stream(Objects.requireNonNull(collectionDirectory.listFiles()))
+                   .filter(resourceDir -> resourceDir.getName().equals(resourceDirectoryName))
+                   .collect(SingletonCollector.collect());
+    }
+
+    private String createKey(Record record, String filename) {
+        var collection = getCollectionDirectory(record.getBrageLocation()).getName();
+        var bundle = getResourceDirectoryName(record.getBrageLocation());
+        var hardCodedCustomer = "nve";
+        return Path.of(hardCodedCustomer, collection, bundle, filename).toString();
+    }
+
+    private File getCollectionDirectory(String brageLocation) {
+        return new File(getPathPrefixString() + brageLocation.split("/")[0]);
+    }
+
+    private void writeRecordToS3(Record record) throws JsonProcessingException {
+        var recordToStore = record.toJsonString();
+        var recordKey = createKey(record, extractHandleForFilename(record));
+        s3Client.putObject(PutObjectRequest
+                               .builder()
+                               .bucket(bucketName)
+                               .contentType(APPLICATION_JSON)
+                               .key(recordKey)
+                               .build(),
+                           RequestBody.fromString(recordToStore));
+    }
+
+    private void writeRecordFilesToS3(Record record) {
+        var filesToStore = getFilesToStore(record);
+        for (UUID fileId : filesToStore.keySet()) {
+            var fileKey = createKey(record, fileId.toString());
+            var fileName = filesToStore.get(fileId).getName();
+            s3Client.putObject(PutObjectRequest
+                                   .builder()
+                                   .bucket(bucketName)
+                                   .contentDisposition(fileName)
+                                   .key(fileKey).build(),
+                               RequestBody.fromFile(filesToStore.get(fileId)));
+        }
+    }
+
+    private Map<UUID, File> getFilesToStore(Record record) {
+        var resourceFiles = findFilesToRecord(record);
+        return mapFilesToUuid(record, resourceFiles);
+    }
+
+    private ConcurrentMap<UUID, File> mapFilesToUuid(Record record, File resourceFiles) {
         ConcurrentMap<UUID, File> map = new ConcurrentHashMap<>();
         Arrays.stream(Objects.requireNonNull(resourceFiles.listFiles()))
             .filter(file -> isPartOfRecord(file, record))
-            .forEach(file -> map.put(getUuid(record, file), file));
+            .forEach(file -> map.put(getUuidToRecordFile(record, file), file));
         return map;
     }
 
     private boolean isPartOfRecord(File file, Record record) {
-        var originalFiles = record.getContentBundle()
-                                .getContentFiles()
-                                .stream()
-                                .map(ContentFile::getFilename)
-                                .collect(Collectors.toList());
-        return originalFiles.contains(file.getName());
+        var recordFiles = record.getContentBundle()
+                              .getContentFiles()
+                              .stream()
+                              .map(ContentFile::getFilename)
+                              .collect(Collectors.toList());
+        return recordFiles.contains(file.getName());
     }
 }
--- post-to-aws/src/test/java/S3RecordStorageTest.java
+++ post-to-aws/src/test/java/S3RecordStorageTest.java
@@ -1,4 +1,10 @@
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static no.sikt.nva.brage.migration.aws.S3RecordStorage.COULD_NOT_WRITE_MESSAGE;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.UUID;
 import no.sikt.nva.brage.migration.aws.S3RecordStorage;
@@ -8,34 +14,65 @@ import no.sikt.nva.brage.migration.common.model.record.Record;
 import no.sikt.nva.brage.migration.common.model.record.Type;
 import no.sikt.nva.brage.migration.common.model.record.content.ContentFile;
 import no.sikt.nva.brage.migration.common.model.record.content.ResourceContent;
+import no.unit.nva.stubs.FakeS3Client;
 import nva.commons.core.paths.UriWrapper;
+import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.Test;
-import testutils.FakeS3ClientWithPutObjectSupport;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 public class S3RecordStorageTest {
 
-    public static final String INVALID_FILE_NAME = "/Users/kir.truhacev/IdeaProjects/nva-brage-migration/samlingsfil"
-                                                   + ".txt";
+    public static final String TEST_PATH = "src/test/resources/";
+    public static final String VALID_TEST_FILE_NAME = "Simulated precipitation fields with variance consistent "
+                                                      + "interpolation.pdf";
+    public static final String TEST_PATH_TO_STORE = "src/test/resources/testFilesToDelete/";
 
     @Test
-    void shouldThrowExceptionWhenInvalidPath() {
-        var s3Client = new FakeS3ClientWithPutObjectSupport("Simulated precipitation fields with variance consistent interpolation.pdf", "11/1", "text/json");
-        var s = new S3RecordStorage(s3Client);
-        s.storeRecord(createTestRecord());
-        assertThrows(Exception.class, () -> s.storeRecord(createTestRecord()));
+    void shouldUploadRecordAndFileToS3File() {
+        Record testRecord = createValidTestRecord();
+        var expectedKeyToRecord = "nve/11/1/1.json";
+        var expectedKeyToFile =
+            "nve/11/1/" + testRecord.getContentBundle().getContentFileByFilename(VALID_TEST_FILE_NAME).getIdentifier();
+        var s3Client = new FakeS3Client();
+        var storageClient = new S3RecordStorage(s3Client, TEST_PATH);
+        storageClient.storeRecord(testRecord);
+        var filenameToStoreRecord = randomString();
+        var filenameToStoreFile = randomString();
+        s3Client.getObject(GetObjectRequest.builder().key(expectedKeyToRecord).build(),
+                           Path.of(TEST_PATH_TO_STORE + filenameToStoreRecord));
+        s3Client.getObject(GetObjectRequest.builder().key(expectedKeyToFile).build(),
+                           Path.of(TEST_PATH_TO_STORE + filenameToStoreFile));
+
+        assertTrue(new File(TEST_PATH_TO_STORE + filenameToStoreRecord).exists());
+        assertTrue(new File(TEST_PATH_TO_STORE + filenameToStoreFile).exists());
     }
 
-    private Record createTestRecord() {
+    @Test
+    void shouldThrowExceptionWhenRecordIsNull() {
+        var appender = LogUtils.getTestingAppenderForRootLogger();
+        var nullRecord = createNullRecord();
+        var s3Client = new FakeS3Client();
+        var storageClient = new S3RecordStorage(s3Client, TEST_PATH);
+        storageClient.storeRecord(nullRecord);
+
+        assertThat(appender.getMessages(), containsString(COULD_NOT_WRITE_MESSAGE));
+    }
+
+    private Record createValidTestRecord() {
         var record = new Record();
         record.setType(new Type(List.of(BrageType.BOOK.getType()), NvaType.BOOK.getValue()));
         record.setPartOf("partOfSomethingBigger");
         record.setCristinId("cristinId");
         record.setBrageLocation("11/1");
         var contentFile = new ContentFile();
-        contentFile.setFilename("Simulated precipitation fields with variance consistent interpolation.pdf");
+        contentFile.setFilename(VALID_TEST_FILE_NAME);
         contentFile.setIdentifier(UUID.randomUUID());
         record.setContentBundle(new ResourceContent(List.of(contentFile)));
         record.setId(UriWrapper.fromUri("https://hdl.handle.net/11/1").getUri());
         return record;
+    }
+
+    private Record createNullRecord() {
+        return new Record();
     }
 }
