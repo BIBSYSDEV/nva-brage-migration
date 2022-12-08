313dc46818c20df554ead0cb95c2d8c2493dd345
Kir Truhacev <kir.truhacev@sikt.no>
2022-12-08 14:55:19 +0100

    done

--- src/main/java/no/sikt/nva/scrapers/ResourceOwnerMapper.java
+++ src/main/java/no/sikt/nva/scrapers/ResourceOwnerMapper.java
@@ -13,7 +13,7 @@ public final class ResourceOwnerMapper {
     @SuppressWarnings("PMD.UseConcurrentHashMap")
     private static final Map<String, ResourceOwner> RESOURCE_OWNER_MAP = Map.ofEntries(
         entry("TEST", new ResourceOwner("TestOwner", UriWrapper.fromUri(TEST).getUri())),
-        entry("NVE", new ResourceOwner(null, UriWrapper.fromUri(NVE).getUri()))
+        entry("NVE", new ResourceOwner("nve@5948.0.0.0", UriWrapper.fromUri(NVE).getUri()))
     );
 
     private ResourceOwnerMapper() {
