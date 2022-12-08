66aaf719e6c7166e1829c1f0deabef36742b32fa
anetteOlli <anette.olli.siiri@sikt.no>
2022-12-08 14:11:11 +0100

    matching-nva-publication-api-datamodel

--- brage-migration-common/src/main/java/no/sikt/nva/brage/migration/common/model/record/PublicationDateNva.java
+++ brage-migration-common/src/main/java/no/sikt/nva/brage/migration/common/model/record/PublicationDateNva.java
@@ -1,6 +1,5 @@
 package no.sikt.nva.brage.migration.common.model.record;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.Objects;
@@ -13,7 +12,10 @@ public final class PublicationDateNva {
     private String month;
     private String day;
 
-    @JsonCreator
+    public PublicationDateNva() {
+        
+    }
+
     private PublicationDateNva(Builder builder) {
         setYear(builder.year);
         setMonth(builder.month);
