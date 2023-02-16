020f3f6b06b75fd112be80da21d6231a3f785f27
Kir Truhacev <kir.truhacev@sikt.no>
2023-02-16 08:51:06 +0100

    duplicate-fix, parsing fails fixed

--- src/test/java/no/sikt/nva/ResourceNameConstants.java
+++ src/test/java/no/sikt/nva/ResourceNameConstants.java
@@ -10,6 +10,8 @@ public class ResourceNameConstants {
     public static final String INPUT_WITHOUT_HANDLE_ZIP_FILE_NAME = "inputWithoutHandle.zip";
     public static final String INPUT_WHERE_DOI_HAS_VALID_STRUCTURE_BUT_HAS_INVALID_URI = "inputWithInvalidDoi.zip";
     public static final String CONTENT_FILE_PATH = "src/test/resources/contents";
+    public static final String EMPTY_CONTENT_FILE_PATH = "src/test/resources/emptyContents";
+
     public static final String EMBARGO_TEST_DIRECTORY = "src/test/resources/resourceWithEmbargo";
 
     public static final String EMPTY_ZIP_FILE_NAME = "empty.zip";
