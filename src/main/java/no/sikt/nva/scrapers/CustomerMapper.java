c91f55d7289f74024148b44b4907ef26abbae77a
Kir Truhacev <kir.truhacev@sikt.no>
2023-07-11 15:11:34 +0200

    more institutions added

--- src/main/java/no/sikt/nva/scrapers/CustomerMapper.java
+++ src/main/java/no/sikt/nva/scrapers/CustomerMapper.java
@@ -17,6 +17,20 @@ public class CustomerMapper {
     public static final String NIBIO = "nibio";
     public static final String NIH = "nih";
     public static final String NTNU = "ntnu";
+    public static final String NR = "nr";
+    public static final String SAMFORSK = "samforsk";
+    public static final String CMI = "cmi";
+    public static final String DMMH = "dmmh";
+    public static final String FAFO = "fafo";
+    public static final String UIA = "uia";
+    public static final String FDIR = "fdir";
+    public static final String NORD = "nord";
+    public static final String ODA = "oda";
+    public static final String HIMOLDE = "himolde";
+    public static final String HIVOLDA = "hivolda";
+    public static final String HVLOPEN = "hvlopen";
+    public static final String NHH = "nhh";
+    public static final String BORA = "bora";
     public static final String SANDBOX = "sandbox";
     public static final String DEVELOP = "dev";
     public static final String TEST = "test";
@@ -45,7 +59,7 @@ public class CustomerMapper {
 
         entry(HIOF, Map.ofEntries(entry(SANDBOX, ""),
                                   entry(DEVELOP, "5c243c2d-a8fb-48fb-a2c6-c6fa70310194"),
-                                  entry(TEST, ""),
+                                  entry(TEST, "719fdb5a-f90a-4e20-b81d-1dde3ca6e647"),
                                   entry(PROD, ""))),
 
         entry(NGI, Map.ofEntries(entry(SANDBOX, ""),
@@ -55,8 +69,8 @@ public class CustomerMapper {
 
         entry(NIBIO, Map.ofEntries(entry(SANDBOX, ""),
                                    entry(DEVELOP, "82c8b036-39cb-4ac2-8a2d-152cda4bcc27"),
-                                   entry(TEST, ""),
-                                   entry(PROD, ""))),
+                                   entry(TEST, "cd925eea-7f4c-4167-9b7c-a7bf7f8eca59"),
+                                   entry(PROD, "77766640-6066-48ef-a32c-0ba7f32abee9"))),
 
         entry(NIH, Map.ofEntries(entry(SANDBOX, ""),
                                  entry(DEVELOP, "b5ff15a1-0e58-44bf-b137-d5c5389ef63f"),
@@ -64,8 +78,66 @@ public class CustomerMapper {
                                  entry(PROD, ""))),
         entry(NTNU, Map.ofEntries(entry(SANDBOX, ""),
                                  entry(DEVELOP, "8ddecceb-7d64-4df3-a842-489fe4d98f3a"),
-                                 entry(TEST, ""),
-                                 entry(PROD, ""))));
+                                 entry(TEST, "33c17ef6-864b-4267-bc9d-0cee636e247e"),
+                                 entry(PROD, ""))),
+        entry(NR, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "54bed9d9-2bc4-4fa4-a6c4-9e957f93870d"),
+                                  entry(TEST, ""),
+                                  entry(PROD, ""))),
+        entry(SAMFORSK, Map.ofEntries(entry(SANDBOX, ""),
+                                entry(DEVELOP, "6da57387-b48a-46dc-a395-d07c12b1e54b"),
+                                entry(TEST, ""),
+                                entry(PROD, ""))),
+        entry(CMI, Map.ofEntries(entry(SANDBOX, ""),
+                                      entry(DEVELOP, "aebc37b9-593c-42b6-88bf-0d8a54492a32"),
+                                      entry(TEST, ""),
+                                      entry(PROD, ""))),
+        entry(DMMH, Map.ofEntries(entry(SANDBOX, ""),
+                                 entry(DEVELOP, "0edc1e9a-a74b-4169-86ba-f98b92d0115e"),
+                                 entry(TEST, "2e4f348c-47a3-4500-94dd-9b4ee4ba63d9"),
+                                 entry(PROD, ""))),
+        entry(FAFO, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "eab76eb2-3f15-40af-9056-53946ec8fcde"),
+                                  entry(TEST, "648a3ed0-f34e-48ff-8db9-092b2f5d5fa2"),
+                                  entry(PROD, ""))),
+        entry(FDIR, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "4403a49b-a175-4985-a695-f16c5b87c9ac"),
+                                  entry(TEST, ""),
+                                  entry(PROD, ""))),
+        entry(UIA, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "8b77046d-2001-47cb-b061-0346d5b4b95c"),
+                                  entry(TEST, "2732ed9a-971a-4e34-ac84-d5f6d067ed66"),
+                                  entry(PROD, "20c7aad5-af76-4909-afe2-cdc9c7ec4f79"))),
+        entry(NORD, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "f94d1d3e-e05a-41f9-8eb8-f891ccd25666"),
+                                  entry(TEST, "27c650e1-9af2-40fd-91c0-3abdf38c86f0"),
+                                  entry(PROD, "0e878881-87b9-4392-b4e4-b410bbecbd1d"))),
+        entry(ODA, Map.ofEntries(entry(SANDBOX, ""),
+                                  entry(DEVELOP, "8db4a8bd-3db5-420b-a2e4-fc17a489aa6d"),
+                                  entry(TEST, ""),
+                                  entry(PROD, ""))),
+        entry(HIMOLDE, Map.ofEntries(entry(SANDBOX, ""),
+                                 entry(DEVELOP, "2927ca20-cbd0-4b0a-abd3-3de23e8f2065"),
+                                 entry(TEST, "e8833011-2e16-4765-b1dd-494b64c8d646"),
+                                 entry(PROD, "9e6b93b6-1ef2-4b91-9599-ebc9ac8c6606"))),
+        entry(HIVOLDA, Map.ofEntries(entry(SANDBOX, ""),
+                                     entry(DEVELOP, "9c81f056-5839-4843-9b93-20962281df38"),
+                                     entry(TEST, "235dc022-6beb-4f8f-8165-9fbe680e1f5b"),
+                                     entry(PROD, "ca410109-2720-475b-a280-59783e2962c7"))),
+        entry(HVLOPEN, Map.ofEntries(entry(SANDBOX, ""),
+                                     entry(DEVELOP, "fe945ea8-22d1-481c-b5be-4e0447d5656c"),
+                                     entry(TEST, "d60e86ef-2bcd-4e0b-8872-43f80858caaa"),
+                                     entry(PROD, "2e3ae52c-ada5-4862-b72a-98e74690465c"))),
+        entry(NHH, Map.ofEntries(entry(SANDBOX, ""),
+                                     entry(DEVELOP, "418e4bf8-6f7e-419c-b7b7-45bcbab38429"),
+                                     entry(TEST, "7bf52e6f-82dc-4203-a25e-93630819b741"),
+                                     entry(PROD, "1432ce2a-5f2e-4560-8b6c-9af87c082d53"))),
+        entry(BORA, Map.ofEntries(entry(SANDBOX, ""),
+                                 entry(DEVELOP, "a228aba6-932b-4f53-b2de-31ad8daf9f8d"),
+                                 entry(TEST, "7c41e7cd-f494-4266-9979-48285cbb2434"),
+                                 entry(PROD, "2f42a7e8-219b-4289-9b41-99b32a6b4866")))
+
+    );
 
     private static final String SANDBOX_URI = "https://api.sandbox.nva.aws.unit.no/customer/";
     private static final String DEVELOP_URI = "https://api.dev.nva.aws.unit.no/customer/";
--- src/main/java/no/sikt/nva/scrapers/ResourceOwnerMapper.java
+++ src/main/java/no/sikt/nva/scrapers/ResourceOwnerMapper.java
@@ -1,6 +1,29 @@
 package no.sikt.nva.scrapers;
 
 import static java.util.Map.entry;
+import static no.sikt.nva.scrapers.CustomerMapper.BORA;
+import static no.sikt.nva.scrapers.CustomerMapper.CMI;
+import static no.sikt.nva.scrapers.CustomerMapper.DMMH;
+import static no.sikt.nva.scrapers.CustomerMapper.FAFO;
+import static no.sikt.nva.scrapers.CustomerMapper.FDIR;
+import static no.sikt.nva.scrapers.CustomerMapper.FHS;
+import static no.sikt.nva.scrapers.CustomerMapper.HIMOLDE;
+import static no.sikt.nva.scrapers.CustomerMapper.HIOF;
+import static no.sikt.nva.scrapers.CustomerMapper.HIVOLDA;
+import static no.sikt.nva.scrapers.CustomerMapper.HVLOPEN;
+import static no.sikt.nva.scrapers.CustomerMapper.KRISTIANIA;
+import static no.sikt.nva.scrapers.CustomerMapper.KRUS;
+import static no.sikt.nva.scrapers.CustomerMapper.NGI;
+import static no.sikt.nva.scrapers.CustomerMapper.NHH;
+import static no.sikt.nva.scrapers.CustomerMapper.NIBIO;
+import static no.sikt.nva.scrapers.CustomerMapper.NIH;
+import static no.sikt.nva.scrapers.CustomerMapper.NORD;
+import static no.sikt.nva.scrapers.CustomerMapper.NR;
+import static no.sikt.nva.scrapers.CustomerMapper.NTNU;
+import static no.sikt.nva.scrapers.CustomerMapper.ODA;
+import static no.sikt.nva.scrapers.CustomerMapper.SAMFORSK;
+import static no.sikt.nva.scrapers.CustomerMapper.NVE;
+import static no.sikt.nva.scrapers.CustomerMapper.UIA;
 import java.net.URI;
 import java.util.Locale;
 import java.util.Map;
@@ -14,33 +37,52 @@ public class ResourceOwnerMapper {
     public static final String DEVELOP = "dev";
     public static final String TEST = "test";
     public static final String PROD = "prod";
-    public static final String NVE = "nve";
     public static final String NVE_OWNER_VALUE = "nve@5948.0.0.0";
     public static final String NVE_CRISTIN_IDENTIFIER = "5948.0.0.0";
-    public static final String KRUS = "krus";
     public static final String KRUS_OWNER_VALUE = "krus@1661.0.0.0";
     public static final String KRUS_CRISTIN_IDENTIFIER = "1661.0.0.0";
-    public static final String KRISTIANIA = "kristiania";
     public static final String KRISTIANIA_OWNER_VALUE = "kristiania@1615.0.0.0";
     public static final String KRISTIANIA_CRISTIN_IDENTIFIER = "1615.0.0.0";
-    public static final String FHS = "fhs";
     public static final String FHS_OWNER_VALUE = "fhs@1627.0.0.0";
     public static final String FHS_CRISTIN_IDENTIFIER = "1627.0.0.0";
-    public static final String HIOF = "hiof";
     public static final String HIOF_OWNER_VALUE = "hiof@224.0.0.0";
     public static final String HIOF_CRISTIN_IDENTIFIER = "224.0.0.0";
-    public static final String NGI = "ngi";
     public static final String NGI_OWNER_VALUE = "ngi@7452.0.0.0";
     public static final String NGI_CRISTIN_IDENTIFIER = "7452.0.0.0";
-    public static final String NIBIO = "nibio";
     public static final String NIBIO_OWNER_VALUE = "nibio@7677.0.0.0";
     public static final String NIBIO_CRISTIN_IDENTIFIER = "7677.0.0.0";
-    public static final String NIH = "nih";
     public static final String NIH_OWNER_VALUE = "nih@150.0.0.0";
     public static final String NIH_CRISTIN_IDENTIFIER = "150.0.0.0";
-    public static final String NTNU = "ntnu";
     public static final String NTNU_OWNER_VALUE = "ntnu@194.0.0.0";
     public static final String NTNU_CRISTIN_IDENTIFIER = "194.0.0.0";
+    public static final String NR_OWNER_VALUE = "nr@7467.0.0.0";
+    public static final String NR_CRISTIN_IDENTIFIER = "7467.0.0.0";
+    public static final String SAMFORSK_OWNER_VALUE = "samforsk@7403.0.0.0";
+    public static final String SAMFORSK_CRISTIN_IDENTIFIER = "7403.0.0.0";
+    public static final String CMI_OWNER_VALUE = "cmi@7416.0.0.0";
+    public static final String CMI_CRISTIN_IDENTIFIER = "7416.0.0.0";
+    public static final String DMMH_OWNER_VALUE = "dmmh@253.0.0.0";
+    public static final String DMMH_CRISTIN_IDENTIFIER = "253.0.0.0";
+    public static final String FAFO_OWNER_VALUE = "fafo@7425.0.0.0";
+    public static final String FAFO_CRISTIN_IDENTIFIER = "7425.0.0.0";
+    public static final String FDIR_OWNER_VALUE = "fdir@5947.0.0.0";
+    public static final String FDIR_CRISTIN_IDENTIFIER = "5947.0.0.0";
+    public static final String UIA_OWNER_VALUE = "uia@201.0.0.0";
+    public static final String UIA_CRISTIN_IDENTIFIER = "201.0.0.0";
+    public static final String NORD_OWNER_VALUE = "nord@204.0.0.0";
+    public static final String NORD_CRISTIN_IDENTIFIER = "204.0.0.0";
+    public static final String ODA_OWNER_VALUE = "oda@novaluepresent";
+    public static final String ODA_CRISTIN_IDENTIFIER = "novaluepresent";
+    public static final String HIMOLDE_OWNER_VALUE = "himolde@211.0.0.0";
+    public static final String HIMOLDE_CRISTIN_IDENTIFIER = "211.0.0.0";
+    public static final String HIVOLDA_OWNER_VALUE = "hivolda@223.0.0.0";
+    public static final String HIVOLDA_CRISTIN_IDENTIFIER = "223.0.0.0";
+    public static final String HVLOPEN_OWNER_VALUE = "hvlopen@203.0.0.0";
+    public static final String HVLOPEN_CRISTIN_IDENTIFIER = "203.0.0.0";
+    public static final String NHH_OWNER_VALUE = "nhh@191.0.0.0";
+    public static final String NHH_CRISTIN_IDENTIFIER = "191.0.0.0";
+    public static final String BORA_OWNER_VALUE = "bora@184.0.0.0";
+    public static final String BORA_CRISTIN_IDENTIFIER = "184.0.0.0";
     public static final String CRISTIN_IDENTIFIER = "CRISTIN_IDENTIFIER";
     public static final String OWNER_VALUE = "OWNER_VALUE";
     public static final String SANDBOX_URI = "https://api.sandbox.nva.aws.unit.no/cristin/organization/";
@@ -66,7 +108,37 @@ public class ResourceOwnerMapper {
         entry(NIH, Map.ofEntries(entry(OWNER_VALUE, NIH_OWNER_VALUE),
                                  entry(CRISTIN_IDENTIFIER, NIH_CRISTIN_IDENTIFIER))),
         entry(NTNU, Map.ofEntries(entry(OWNER_VALUE, NTNU_OWNER_VALUE),
-                                  entry(CRISTIN_IDENTIFIER, NTNU_CRISTIN_IDENTIFIER))));
+                                  entry(CRISTIN_IDENTIFIER, NTNU_CRISTIN_IDENTIFIER))),
+        entry(NR, Map.ofEntries(entry(OWNER_VALUE, NR_OWNER_VALUE),
+                                entry(CRISTIN_IDENTIFIER, NR_CRISTIN_IDENTIFIER))),
+        entry(SAMFORSK, Map.ofEntries(entry(OWNER_VALUE, SAMFORSK_OWNER_VALUE),
+                                entry(CRISTIN_IDENTIFIER, SAMFORSK_CRISTIN_IDENTIFIER))),
+        entry(CMI, Map.ofEntries(entry(OWNER_VALUE, CMI_OWNER_VALUE),
+                                      entry(CRISTIN_IDENTIFIER, CMI_CRISTIN_IDENTIFIER))),
+        entry(DMMH, Map.ofEntries(entry(OWNER_VALUE, DMMH_OWNER_VALUE),
+                                 entry(CRISTIN_IDENTIFIER, DMMH_CRISTIN_IDENTIFIER))),
+        entry(FAFO, Map.ofEntries(entry(OWNER_VALUE, FAFO_OWNER_VALUE),
+                                  entry(CRISTIN_IDENTIFIER, FAFO_CRISTIN_IDENTIFIER))),
+        entry(FDIR, Map.ofEntries(entry(OWNER_VALUE, FDIR_OWNER_VALUE),
+                                  entry(CRISTIN_IDENTIFIER, FDIR_CRISTIN_IDENTIFIER))),
+        entry(UIA, Map.ofEntries(entry(OWNER_VALUE, UIA_OWNER_VALUE),
+                                  entry(CRISTIN_IDENTIFIER, UIA_CRISTIN_IDENTIFIER))),
+        entry(NORD, Map.ofEntries(entry(OWNER_VALUE, NORD_OWNER_VALUE),
+                                 entry(CRISTIN_IDENTIFIER, NORD_CRISTIN_IDENTIFIER))),
+        entry(ODA, Map.ofEntries(entry(OWNER_VALUE, ODA_OWNER_VALUE),
+                                  entry(CRISTIN_IDENTIFIER, ODA_CRISTIN_IDENTIFIER))),
+        entry(HIMOLDE, Map.ofEntries(entry(OWNER_VALUE, HIMOLDE_OWNER_VALUE),
+                                 entry(CRISTIN_IDENTIFIER, HIMOLDE_CRISTIN_IDENTIFIER))),
+        entry(HIVOLDA, Map.ofEntries(entry(OWNER_VALUE, HIVOLDA_OWNER_VALUE),
+                                     entry(CRISTIN_IDENTIFIER, HIVOLDA_CRISTIN_IDENTIFIER))),
+        entry(HVLOPEN, Map.ofEntries(entry(OWNER_VALUE, HVLOPEN_OWNER_VALUE),
+                                     entry(CRISTIN_IDENTIFIER, HVLOPEN_CRISTIN_IDENTIFIER))),
+        entry(NHH, Map.ofEntries(entry(OWNER_VALUE, NHH_OWNER_VALUE),
+                                     entry(CRISTIN_IDENTIFIER, NHH_CRISTIN_IDENTIFIER))),
+        entry(BORA, Map.ofEntries(entry(OWNER_VALUE, BORA_OWNER_VALUE),
+                                 entry(CRISTIN_IDENTIFIER, BORA_CRISTIN_IDENTIFIER)))
+
+    );
 
     private static final Map<String, String> ENVIRONMENT_URI_MAP = Map.ofEntries(
         entry(SANDBOX, SANDBOX_URI),
