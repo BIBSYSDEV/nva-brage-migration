f185842b9be32ed096b13bef4514bc0f3c6f3606
Kir Truhacev <kir.truhacev@sikt.no>
2024-06-26 08:55:57 +0200

    extending doi validator

--- src/test/java/no/sikt/nva/validators/DoiValidatorTest.java
+++ src/test/java/no/sikt/nva/validators/DoiValidatorTest.java
@@ -21,13 +21,15 @@ public class DoiValidatorTest {
         return Stream.of(
             Arguments.of("DOI:10.1371/journal.pone.0118594"),
             Arguments.of("doi:10.1371/journal.pone.0118594"),
-        Arguments.of("DOI:10.1371/journal.pone.0125743"),
-        Arguments.of("https://doi.org/10.5194/nhess-2018-174"),
-        Arguments.of("doi.org/10.5194/nhess-2018-174"),
-        Arguments.of("10.5194/nhess-2018-174"),
-        Arguments.of("https://doi.org/10.1577/1548-8667(1998)010<0056:EOOAFI>2.0.CO;2"),
-        Arguments.of("https://doi.org/10.1890/0012-9658(2006)87[2915:DMWITM]2.0.CO;2"),
-        Arguments.of("https://doi.org/10.2983/0730-8000(2008)27[525:EOAMRF]2.0.CO;2")
+            Arguments.of("DOI:10.1371/journal.pone.0125743"),
+            Arguments.of("https://doi.org/10.5194/nhess-2018-174"),
+            Arguments.of("doi.org/10.5194/nhess-2018-174"),
+            Arguments.of("10.5194/nhess-2018-174"),
+            Arguments.of("https://doi.org/10.1577/1548-8667(1998)010<0056:EOOAFI>2.0.CO;2"),
+            Arguments.of("https://doi.org/10.1890/0012-9658(2006)87[2915:DMWITM]2.0.CO;2"),
+            Arguments.of("https://doi.org/10.2983/0730-8000(2008)27[525:EOAMRF]2.0.CO;2"),
+            Arguments.of("https:/doi.org/10.1080/08039410.2015.1114517"),
+            Arguments.of("http://dx.doi.org/10.5334/ah.be")
         );
     }
 
