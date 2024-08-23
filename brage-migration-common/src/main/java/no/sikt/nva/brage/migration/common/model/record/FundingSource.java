4a4b85e2a434494ce4150984ddeb0d5638dfc123
Kir Truhacev <kir.truhacev@sikt.no>
2024-08-23 12:33:13 +0200

    fixing codacy

--- brage-migration-common/src/main/java/no/sikt/nva/brage/migration/common/model/record/FundingSource.java
+++ brage-migration-common/src/main/java/no/sikt/nva/brage/migration/common/model/record/FundingSource.java
@@ -3,7 +3,6 @@ package no.sikt.nva.brage.migration.common.model.record;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import java.net.URI;
 import java.util.Map;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
